a74f555d08c7754a641e455864c2b0deb2b11abb
carvalho.rac <carvalho.rac@dpf.gov.br>
2020-11-10 07:58:02 -0300

    menu escondendo

--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./default.component.css']
 })
 export class DefaultComponent implements OnInit {
-
+  
+  sideBarOpen = true;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  sideBarToggler(){
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,7 +8,9 @@ import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule} from '@angular/material/icon';
 import { MatButtonModule} from '@angular/material/button';
 import { MatMenuModule} from '@angular/material/menu';
+import { MatListModule} from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -24,7 +26,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatIconModule,
     MatButtonModule,
     FlexLayoutModule,
-    MatMenuModule
+    MatMenuModule,
+    MatListModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent,
